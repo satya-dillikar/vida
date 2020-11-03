153b9cde75b6077cc95e91289f82a32de4ddd8c7
hemantgit123 <55827895+hemantgit123@users.noreply.github.com>
2020-11-03 17:55:08 +0530

    Update deploy-helm.py

--- deploy-helm.py
+++ deploy-helm.py
@@ -9,12 +9,23 @@ port = 10004
 username = sys.argv[2]
 password = sys.argv[3]
 
-command = "helm delete wp; helm repo update;  helm install wp hb/wordpress-mysql-stateless"
+command1 = "helm delete wp; helm repo update"
+command2 = "helm install wp hb/wordpress-mysql-stateless"
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(host, port, username, password)
-stdin, stdout, stderr = ssh.exec_command(command)
+stdin, stdout, stderr = ssh.exec_command(command1)
+stdin.close()
+outlines=stdout.readlines()
+resp=''.join(outlines)
+print(resp)
+
+stdout.close()
+stdin.close()
+ssh.close()
+
+stdin, stdout, stderr = ssh.exec_command(command2)
 stdin.close()
 outlines=stdout.readlines()
 resp=''.join(outlines)
