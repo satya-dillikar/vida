16fe09a366ee287dd441a40267c8458993ffe4f9
hemantgit123 <55827895+hemantgit123@users.noreply.github.com>
2020-11-18 10:34:01 +0530

    Create deploy-helm-statefull-cluster1.py

---
+++ deploy-helm-statefull-cluster1.py
@@ -0,0 +1,33 @@
+import re
+import os
+import time
+os.system('pip3 install paramiko')
+import paramiko
+import sys
+import json
+
+#Opening config file....
+
+with open('./resource-sandboxInfo/inputs/sandbox1_info.json') as f:
+  data = json.load(f)
+
+
+host = data["ip"]
+port = data["port"]
+username = data["username"]
+password = data["password"]
+
+
+
+command = "export KUBECONFIG=/home/vmware/kubeconfig.yaml; helm --kubeconfig=/home/vmware/kubeconfig.yaml delete wp; helm repo update;helm --kubeconfig=/home/vmware/kubeconfig.yaml --set storageClassName=standard install wp hb_ec2/wordpress"
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+stdin, stdout, stderr = ssh.exec_command(command)
+print (stdout.read())
+time.sleep(10)
+
+stdout.close()
+stdin.close() 
+ssh.close()
